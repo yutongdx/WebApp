ad9f75be63e374a020d4cf60df775857da21eaf8
LQL8023YOU <1057699668@qq.com>
2021-07-15 23:24:00 +0800

    修改布局名称，优化代码结构

--- app/src/main/java/com/hailing/webapp/ui/main/BookmarkFragment.java
+++ app/src/main/java/com/hailing/webapp/ui/main/BookmarkFragment.java
@@ -1,4 +1,4 @@
-package ui.main;
+package com.hailing.webapp.ui.main;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,9 +11,10 @@ import androidx.fragment.app.Fragment;
 
 import com.hailing.webapp.R;
 
-public class HomeFragment extends Fragment {
+// 此碎片实现书签功能
+public class BookmarkFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_bookmark, container, false);
         return view;
     }
 }
