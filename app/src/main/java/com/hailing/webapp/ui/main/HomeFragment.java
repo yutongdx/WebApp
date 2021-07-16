0f6b69b42f843238124161eaaa1960ca12e25d9a
yuTong <unicon615@gmail.com>
2021-07-16 16:31:31 +0800

    feat(ui & res):基本实现首页和浏览页面功能

--- app/src/main/java/com/hailing/webapp/ui/main/HomeFragment.java
+++ app/src/main/java/com/hailing/webapp/ui/main/HomeFragment.java
@@ -1,19 +1,62 @@
 package com.hailing.webapp.ui.main;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.hailing.webapp.R;
+import com.hailing.webapp.ui.browse.BrowseActivity;
 
 public class HomeFragment extends Fragment {
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        WebView webView = view.findViewById(R.id.home_web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);   //设定webView允许使用JavaScript
+        webSettings.setSupportZoom(true);      //设定webView允许使用缩放手势
+        webSettings.setLoadWithOverviewMode(true);  //设定webView以概述模式加载页面
+
+        webView.loadUrl("https://cn.bing.com/");  //主页加载百度首页
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                if (url == null) return false;
+
+                try{
+                    //不是打开网页则调用系统浏览器打开
+                    if(!url.startsWith("http://") && !url.startsWith("https://")){
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                        return false;
+                    }
+                }catch (Exception e){//防止crash (如果手机上没有安装处理某个scheme开头的url的APP, 会导致crash)
+                    return true;//没有安装该app时，返回true，表示拦截自定义链接，但不跳转，避免弹出上面的错误页面
+                }
+
+                // TODO Auto-generated method stub
+                if (url.contains("FORM=BEHPTB")) {
+                    webView.loadUrl(url);
+                } else { //跳转去浏览页面打开
+                    BrowseActivity.actionStart(getActivity(), "homeFragment", url);
+                }
+                return true;
+            }
+        });
+
         return view;
     }
 }
